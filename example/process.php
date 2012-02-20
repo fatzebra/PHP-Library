758ea2e87cbde8547b8ce732de892e006932053b
Matthew Savage <matt@amasses.net>
2012-02-20 17:08:00 +1100

    Added examples

---
+++ example/process.php
@@ -0,0 +1,19 @@
+<?php
+  session_start();
+  include_once("../FatZebra.class.php");
+  define("USERNAME", "havanaco");
+  define("TOKEN", "673bb3aaca9a1961bfa3c61917594dc7c4a00b71");
+  define("TEST_MODE", true);
+
+  try {
+  	$gateway = new FatZebra\Gateway(USERNAME, TOKEN, TEST_MODE);
+  	$purchase_request = new FatZebra\PurchaseRequest($_POST['amount'], $_POST['reference'], $_POST['name'], $_POST['card_number'], $_POST['card_expiry_month'] ."/". $_POST['card_expiry_month'], $_POST['card_cvv']);
+
+  	$response = $gateway->purchase($purchase_request);
+
+  	$_SESSION['response'] = $response;
+  	header("Location: index.php");
+	} catch(Exception $ex) {
+		print "Error: " . $ex->getMessage();
+	}
+?>
