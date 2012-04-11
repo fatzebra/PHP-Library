23972eafa7349a8645c5077541325a9bacf97784
Matthew Savage <matt@amasses.net>
2012-04-11 13:10:03 +1000

    updated test to use versioned URL

--- test/gateway_tests.php
+++ test/gateway_tests.php
@@ -7,7 +7,6 @@
 
 		public function test_valid_transaction() {
 			$gw = new FatZebra\Gateway("TEST", "TEST", true, "https://gateway.sandbox.fatzebra.com.au");
-			$gw->api_version = null;
 
 			$req = new FatZebra\PurchaseRequest(100.00, "UNITTEST" . rand(), "Jim Smith", "5123456789012346", "05/2013", 123);
 			$result = $gw->purchase($req);
